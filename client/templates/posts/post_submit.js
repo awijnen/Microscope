02854577492de4ab0894e99323635e3865cd11c5
Anthony Wijnen <anthony.wijnen@gmail.com>
2014-12-16 12:06:02 +0100

    ch7_5 done

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,7 +7,7 @@ Template.postSubmit.events({
 			title: templ.find('input[name=title]').value
 		};
 
-		Meteor.call('postInsert', post, function(error, result) {
+		var result = Meteor.call('postInsert', post, function(error, result) {
 			if (error) {
 				return alert(error.reason);
 			}
@@ -15,8 +15,8 @@ Template.postSubmit.events({
 			if (result.postExists) {
 				alert('This link has already been posted');
 			}
-
-			Router.go('postPage', {_id: result._id});
 		});
+
+		Router.go('postsList');
 	}
 });
